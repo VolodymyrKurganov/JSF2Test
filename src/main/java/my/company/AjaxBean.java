a1537856c10a8bf14b4fb0d0cd16a862e6ce31b1
Vladimir Batkovich <vladimir.kurganov@gmail.com>
2011-10-07 17:54:48 +0300

    minor update

--- src/main/java/my/company/AjaxBean.java
+++ src/main/java/my/company/AjaxBean.java
@@ -2,17 +2,26 @@ package my.company;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
 
 @SessionScoped
 @ManagedBean
-public class AjaxBean {
+public class AjaxBean implements Serializable{
     private int counter = 0;
 
     public int getCounter() {
         return counter;
     }
 
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
     public void increase() {
         counter++;
+    }
+
+    public void showError() {
+        throw new RuntimeException("bubug");
     }
 }
